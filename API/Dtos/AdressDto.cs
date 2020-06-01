5c225499e8eebccbd35d525f07ba7fa86dce5668
unknown <neubauersim@gmail.com>
2020-06-01 17:28:51 +0200

    update

--- API/Dtos/AdressDto.cs
+++ API/Dtos/AdressDto.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class AdressDto
     {
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
         public string State { get; set; }
+
+        [Required]
         public string Zipcode { get; set; }
 
     }
--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -13,6 +13,26 @@ namespace API.Extensions
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("1.0.0", new OpenApiInfo { Title = "Skinet API", Version = "1.0.0" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id ="Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    {securitySchema, new []{"Bearer"}}};
+                    c.AddSecurityRequirement(securityRequirement);
             });
 
             return services;

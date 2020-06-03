8e9aaaf57705264844ff6ebb47fedd4af7e90a5a
unknown <neubauersim@gmail.com>
2020-06-03 10:08:25 +0200

    auth guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ProductDetailsComponent } from './shop/product-details/product-details.
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 
 const routes: Routes = [
@@ -14,7 +15,7 @@ const routes: Routes = [
   {path: 'not-found', component: NotFoundComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule)},
-  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule)},
+  {path: 'checkout', canActivate: [AuthGuard], loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule)},
   {path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -31,14 +31,12 @@ export class AppComponent implements OnInit {
 
   loadCurrentUser() {
     const token = localStorage.getItem('token');
-    if (token) {
-      this.accountService.loadCurrentUser(token)
-        .subscribe(() => {
-          console.log('loaded User');
-        }, error => {
-          console.log(error);
-        });
-    }
+    this.accountService.loadCurrentUser(token)
+      .subscribe(() => {
+        console.log('loaded User');
+      }, error => {
+        console.log(error);
+      });
   }
 
 }
